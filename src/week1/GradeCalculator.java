78b0f4b8d4c7a44cf6d5fa0c50373e41cf26a434
kahramanukten <kahraman.ukten@getir.com>
2021-09-01 13:08:59 +0300

    deneme

--- src/week1/GradeCalculator.java
+++ src/week1/GradeCalculator.java
@@ -29,7 +29,37 @@ public class GradeCalculator {
 
         System.out.println("Your grade is " + grade);
 
+        if (grade < 50)
+        {
 
+            System.out.println("Your letter grade is F");
+
+        }
+        else if (grade >= 50 && grade < 60) {
+
+            System.out.println("Your latter grade is D");
+
+        }
+        else if (grade >= 60 && grade < 70) {
+
+            System.out.println("Your latter grade is c");
+
+        }
+        else if (grade >= 70 && grade < 80) {
+
+            System.out.println("Your letter grade is C");
+
+        }
+        else if (grade >= 80 && grade < 90) {
+
+            System.out.println("Your letter grade is B");
+
+        }
+        else if (grade >= 90 && grade < 100) {
+
+            System.out.println("Your letter grade is A");
+
+        }
 
     }
 }
