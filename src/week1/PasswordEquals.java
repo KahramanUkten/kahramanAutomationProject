6ddb3f63a3434d3e74e9cf3ac7c6fad32217682b
kahramanukten <kahraman.ukten@getir.com>
2021-09-02 14:29:27 +0300

    ekleme

---
+++ src/week1/PasswordEquals.java
@@ -0,0 +1,26 @@
+package week1;
+
+import java.util.Scanner;
+
+public class PasswordEquals {
+
+    public static void main(String[] args){
+        String password = "Kahraman34";
+        System.out.println("Please enter a password");
+        Scanner scanner = new Scanner(System.in);
+        String enteredPassword = scanner.nextLine();
+
+        if (enteredPassword.equals(password)){
+
+            System.out.println("Giriş başarılı!!!");
+
+        }
+
+        else {
+
+            System.out.println("Giriş başarısız!!!");
+
+        }
+
+    }
+}
